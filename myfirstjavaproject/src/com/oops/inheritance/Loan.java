58d6f0ccff66a7b1b077d710d6b27513d66137ae
Srikanth <srikanthjava91@gmail.com>
2026-02-09 19:07:44 +0530

    Personal Loan info

---
+++ myfirstjavaproject/src/com/oops/inheritance/Loan.java
@@ -0,0 +1,100 @@
+package com.oops.inheritance;
+
+import java.util.Scanner;
+
+public class Loan {
+	static Scanner sc = new Scanner(System.in);
+
+	boolean validateAadher() {
+		System.out.println("Enter your Aadhar Number: ");
+		String aadhar = sc.next();
+
+		if (aadhar.matches("^[2-9][0-9]{11}$")) {
+			System.out.println("✅ Valid Aadhar Number");
+			return true;
+		} else {
+			System.out.println("❌ Invalid Aadhar Number (Must be 12 digits)");
+			return false;
+		}
+	}
+
+	boolean ValidatePan() {
+
+		System.out.println("Enter your PAN Number: ");
+		String aadhar = sc.next();
+
+		if (aadhar.matches("^[A-Z]{5}[0-9]{4}[A-Z]{1}$")) {
+			System.out.println("✅ Valid PAN Number");
+			return true;
+		} else {
+			System.out.println("❌ Invalid PAN Number (Must be like an Example :ABCDE1234F)");
+			return false;
+		}
+
+	}
+
+	boolean validatePhone() {
+
+		System.out.println("Enter your Phone Number: ");
+		String phone = sc.next();
+
+		if (phone.matches("^[6-9][0-9]{9}$")) {
+			System.out.println("✅ Valid phone Number");
+			return true;
+		} else {
+			System.out.println("❌ Invalid phone Number (Must be 10 digits)");
+			return false;
+		}
+
+	}
+
+	double getUserSalaryInfo() {
+		System.out.println("Enter your salary info : ");
+		double sal = sc.nextDouble();
+		return sal;
+	}
+
+	int getUserCibilScore() {
+		System.out.println("Enter Your cibil score : ");
+		int cibil = sc.nextInt();
+		return cibil;
+	}
+
+	int getUserAge() {
+		System.out.println("Enter your age : ");
+		int age = sc.nextInt();
+		return age;
+	}
+
+	double getROI() {
+		double roi = 9.5;
+		return roi;
+	}
+
+	String getAddressDetails() {
+		String address = "";
+
+		System.out.println("Enter your flat no : ");
+		sc.nextLine();
+		String flatno = sc.nextLine();
+
+		System.out.println("Enter plot details: ");
+		String plot = sc.nextLine();
+
+		System.out.println("Enter Apartment name  ");
+		String aptName = sc.nextLine();
+
+		System.out.println("Enter your street : ");
+		String street = sc.nextLine();
+
+		System.out.println("Enter pinocde :");
+		long pin = sc.nextLong();
+
+		address = "Flat No : " + flatno + " " + "Plot Details : " + plot + " " + "Street Name : " + street + " "
+				+ "apartment name : " + aptName + " " + "pincode : " + pin;
+
+		return address;
+
+	}
+
+}
---
+++ myfirstjavaproject/src/com/oops/inheritance/PersonalLoan.java
@@ -0,0 +1,38 @@
+package com.oops.inheritance;
+
+import java.util.Scanner;
+
+public class PersonalLoan extends Loan {
+	static Scanner sc = new Scanner(System.in);
+
+	void getPersonalLoanDocInfo() {
+		System.out.println("Successfully submitted all documents : ");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Vcube Loans !!");
+		System.out.println("Welcome to Personal Loan Banking");
+
+		// By using Child Object Child reference we can access both Parent and child
+		// class methods directly
+		PersonalLoan pl = new PersonalLoan();
+
+		double salary = pl.getUserSalaryInfo();
+		int cibil = pl.getUserCibilScore();
+		int age = pl.getUserAge();
+
+		if (salary >= 800000.00 && cibil >= 750 && age >= 26 && pl.validateAadher() && pl.ValidatePan()
+				&& pl.validatePhone()) {
+			System.out.println("Congratulations !! You are eligible for Personal Loan !");
+			System.out.println("Your Personal Loan ROI is : " + pl.getROI());
+			System.out.println("Address details : ");
+			String address = pl.getAddressDetails();
+
+			pl.getPersonalLoanDocInfo();
+
+			System.out.println("Entered address is : " + address);
+		} else {
+			System.out.println("You are not eligible for Personal Loan !! Better luck Next time : !");
+		}
+	}
+}
